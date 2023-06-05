c57650af56e561ff23b6f6a5f226b3b756cc2dfa
JuanValero <juanvalero252@gmail.com>
2023-06-04 23:21:50 -0300

    PresignedURL for AWS S3

--- recetas/src/main/java/com/distribuidas/recetas/controlador/MultimediaController.java
+++ recetas/src/main/java/com/distribuidas/recetas/controlador/MultimediaController.java
@@ -1,14 +1,12 @@
 package com.distribuidas.recetas.controlador;
 
 import com.distribuidas.recetas.modelo.entities.Multimedia;
+import com.distribuidas.recetas.multimedia.MultimediaCloudHandler;
 import com.distribuidas.recetas.servicios.interfaces.MultimediaService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
@@ -17,9 +15,15 @@ import java.util.Collection;
 @AllArgsConstructor
 public class MultimediaController {
     private MultimediaService multimediaService;
+    private MultimediaCloudHandler multimediaCloudHandler;
 
     @PostMapping
     public ResponseEntity<?> cargarMultimedia(@RequestBody Collection<Multimedia> multimediaByIdPaso) {
         return new ResponseEntity<>(this.multimediaService.cargarMultimediasPaso(multimediaByIdPaso), HttpStatus.OK);
+    }
+
+    @GetMapping("/get_url")
+    public String GetCertifiedURL(@RequestParam("file_name") String FileRequest,@RequestHeader("user_id") String UserID){
+        return multimediaCloudHandler.GetURlSignedForUpload(FileRequest,UserID);
     }
 }
